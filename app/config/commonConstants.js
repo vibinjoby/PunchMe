c5237388526c8e0f033f7593c9a539a188e92909
System Administrator <root@Lijoshs-MacBook-Pro.local>
2020-07-20 15:49:44 -0400

    adding constant changes

---
+++ app/config/commonConstants.js
@@ -0,0 +1,30 @@
+export default Object.freeze({
+  CLOCK_INITIAL_ZERO: "00",
+  SETTINGS_LINK: [
+    {
+      id: 1,
+      title: "Help Center",
+      imgUri: require("../assets/helpCenter.png"),
+    },
+    {
+      id: 2,
+      title: "Report a Problem",
+      imgUri: require("../assets/report.png"),
+    },
+    {
+      id: 3,
+      title: "Terms and Policies",
+      imgUri: require("../assets/terms.png"),
+    },
+    {
+      id: 4,
+      title: "About Us",
+      imgUri: require("../assets/about.png"),
+    },
+    {
+      id: 5,
+      title: "Contact Us",
+      imgUri: require("../assets/contact.png"),
+    },
+  ],
+});
